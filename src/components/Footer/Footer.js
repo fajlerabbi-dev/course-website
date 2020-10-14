104e78cd5bcd9043c4a859040cfd61d74c815536
Fajle Rabbi <fajlerabbi@Fajles-iMac.local>
2020-10-14 09:45:33 +0600

    Added footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./Footer.css"
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className="copyright">
+			<div className="container">
+				<div className="row">
+					<div className="col-12">
+						<footer>
+							<span>Footer copyright </span>
+						</footer>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
